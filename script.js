7388a4cde2792bf66b14ce55c70d0aae7088dc76
Pradeep Kumar <95734296+Impradeep2000@users.noreply.github.com>
2023-08-18 19:45:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 function calculateMinCost() {
   //your code here
-  
-  
-  
+	let arr = [4, 2, 7, 6, 9];
+	arr.sort((a-b)=>a-b);
+	let result = arr.reduce((preElement,element,index)=>{
+		return preElement + element;
+	}, 0)
+	console.log(result);
 }  
