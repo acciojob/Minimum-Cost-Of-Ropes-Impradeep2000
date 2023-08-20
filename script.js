d25309be909cbfa786738c3550467f8bb8e1612f
Pradeep Kumar <95734296+Impradeep2000@users.noreply.github.com>
2023-08-20 20:49:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
-function calculateMinCost() {
-  //your code here
-	  let ans = 0,
-	  arr = document
+ function calculateMinCost() {
+  let ans = 0,
+    arr = document
       .getElementById("rope-lengths")
       .value.split(",")
       .map((val) => parseInt(val));
-      while (arr.length > 1) {
-        arr.sort((a, b) => a - b);
-        arr[1] += arr[0];
-        ans += arr[1];
-        arr.shift();
-      }
+  while (arr.length > 1) {
+    arr.sort((a, b) => a - b);
+    arr[1] += arr[0];
+    ans += arr[1];
+    arr.shift();
+  }
   document.getElementById("result").innerText = ans;
-}  
+}
+var n = prompt("Enter the number of elements");
+var arr = [];
+for (var i = 0; i < n; i++) {
+    arr[i] = prompt("Enter element " + (i+1));
+}
+console.log( calculateMinCost(arr, n));
